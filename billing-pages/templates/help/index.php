59d370a212bc55c85ad3a65637994a5a6d9515bc
DBilke <info@dominic-bilke.de>
2025-07-07 18:22:50 +0200

    fix help page

--- billing-pages/templates/help/index.php
+++ billing-pages/templates/help/index.php
@@ -4,7 +4,7 @@
  */
 ?>
 <!DOCTYPE html>
-<html lang="<?= $localization->getCurrentLanguage() ?>">
+<html lang="<?= $localization->getLocale() ?>">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
